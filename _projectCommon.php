444adc5fb4cff369e70e898ca40af5ec693ab2e9
oliviert <oliviert>
2006-01-12 16:07:52 +0000

    HEAD - Replace Subprojects with Components

--- _projectCommon.php
+++ _projectCommon.php
@@ -9,7 +9,7 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	$Nav->addNavSeparator("Subprojects", 	"main.php");
+	$Nav->addNavSeparator("Components", 	"main.php");
 	$Nav->addCustomNav("APT", 	"http://www.eclipse.org/jdt/apt/index.html", "_self", 1);
 	$Nav->addCustomNav("Core", 	"http://www.eclipse.org/jdt/core/index.html", "_self", 1);
 	$Nav->addCustomNav("Debug", "http://dev.eclipse.org/viewcvs/index.cgi/%7Echeckout%7E/jdt-debug-home/index.html", "_self", 1);
