54d17f1ba2193df92057ae3f94a40b5c60d31ced
oliviert <oliviert>
2006-01-17 02:00:53 +0000

    *** empty log message ***

---
+++ core/update-site/index.php
@@ -0,0 +1,48 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# sample_3col.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-11-07
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Eclipse update-site";
+	$pageKeywords	= "Eclipse, update, site, cheetah";
+	$pageAuthor		= "";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		Back to <a href="../dev.php">JDT Core Component Development Resources page</a>.
+		<p>This is an Eclipse update-site.
+		<br>
+		From within Eclipse, use:<br>
+		Help&nbsp;<b>></b>&nbsp;Software&nbsp;Updates&nbsp;<b>></b>&nbsp;Find&nbsp;and&nbsp;Install&nbsp;<b>></b>&nbsp;Search&nbsp;for&nbsp;new&nbsp;features&nbsp;to&nbsp;install&nbsp;<b>></b>&nbsp;...
+		</p>
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
