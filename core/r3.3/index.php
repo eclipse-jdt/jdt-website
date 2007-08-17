e5e931fe27cc59bf92c7a5aa29d2f89fa790a057
jeromel <jeromel>
2007-08-17 11:46:23 +0000

    3.3.1 patch

--- core/r3.3/index.php
+++ core/r3.3/index.php
@@ -303,7 +303,7 @@ ob_start();
           </tr>
 	  </table>
 -->	  
-			<p><img src="http://dev.eclipse.org/images/Adarrow.gif" nosave="" border="0" height="16" width="16">&nbsp;<b>3.4 integration preview </b>
+			<p><img src="http://dev.eclipse.org/images/Adarrow.gif" nosave="" border="0" height="16" width="16">&nbsp;<b>3.3.1 preview </b>
       <table>
           <tr>
 			<td valign="top"><a href="../patches/org.eclipse.jdt.core_3.3.1.z20070817-1335.jar">
