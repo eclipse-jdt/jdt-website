8947f2a312dc770e81b86234c6c6e593ea7d2010
oliviert <oliviert>
2010-09-08 12:29:26 +0000

    HEAD - Update plan revised date

--- core/r3.7/plan.php
+++ core/r3.7/plan.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	#
 	# Begin: page-specific settings.  Change these. 
-	$pageTitle 		= "JDT Core 3.6 Plan";
+	$pageTitle 		= "JDT Core 3.7 Plan";
 	$pageKeywords	= "compiler, classpath, Java, plan, planning, JDT, jdt, development, tools, ide, Eclipse";
 	$pageAuthor		= "The JDT Core Team";
 	
