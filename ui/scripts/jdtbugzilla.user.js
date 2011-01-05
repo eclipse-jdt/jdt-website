cbd2af46d98f1cc583afbd97b2124ae0570bb2cf
mkeller <mkeller>
2011-01-05 18:07:38 +0000

    fix attachment links (revert the new Bugzilla 3.6 "feature" that shows fancy patch viewer but kills copy/paste of patch or patch URL into Eclipse):

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -379,9 +379,11 @@ function createCategoryChoosers() {
 
 // Remove Eclipse ads:
 var headerElem= document.getElementById("header");
-var tableElem= headerElem.getElementsByTagName("table");
-if (headerElem && tableElem) {
-    headerElem.removeChild(tableElem[0]);
+if (headerElem) {
+    var tableElem= headerElem.getElementsByTagName("table");
+    if (tableElem) {
+        headerElem.removeChild(tableElem[0]);
+    }
 }
 var headerIconsElem= document.getElementById("header-icons");
 if (headerIconsElem) {
@@ -791,12 +793,27 @@ if (window.location.pathname.match(/.*enter_bug\.cgi/)) {
 	
 	// Loop over <a>s:
 	var anchors= document.getElementsByTagName("a");
+	var detailsRegex= /attachment\.cgi\?id=(\d+)&action=edit/; // attachment.cgi?id=146395&amp;action=edit
+	var diffRegex   = /attachment\.cgi\?id=(\d+)&action=diff/;
 	var commentRegex= /^c(\d+)$/; // c42
 	for (var i= 0; i < anchors.length; i++) {
 	    var aElem= anchors[i];
 	    
+	    // Add [diff] after [details] in attachment references:
+	    if (aElem.textContent == "[details]" && aElem.href.search(detailsRegex) != -1) {
+	        var diffElem= document.createElement("a");
+	        diffElem.textContent= "[diff]";
+	        diffElem.href= aElem.href.replace(detailsRegex, "attachment.cgi?id=$1&action=diff");
+	        aElem.parentNode.appendChild(document.createTextNode(" "));
+	        aElem.parentNode.appendChild(diffElem);
+	        i++; //skip new anchor
+	        
+	    // Fix attachment link (revert the new Bugzilla 3.6 "feature" that shows fancy patch viewer but kills copy/paste of patch into Eclipse):
+	    } else if (aElem.name.substr(0, 7) == "attach_" && aElem.href.search(diffRegex) != -1) {
+	        aElem.setAttribute("href", aElem.href.replace(diffRegex, "attachment.cgi?id=$1"))
+	    
 	    // Change "Comment 42" to ">bug 170000 comment 42<" (simplifies copy/paste of reference):
-	    if (aElem.name.match(commentRegex)) {
+	    } else if (aElem.name.match(commentRegex)) {
 	        aElem.textContent= "comment " + commentRegex.exec(aElem.name)[1];
 	        
 	        var pre= document.createTextNode("bug " + bugId + " ");
