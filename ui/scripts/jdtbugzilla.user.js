5d32de4ef4e2168508c62622d842637f767bf656
mkeller <mkeller>
2011-05-05 10:36:21 +0000

    3.7 RC1

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -28,7 +28,7 @@
 
 
 // Add as many milestones as you like. First will be used for "Fixed (in <TM>)" link:
-var target_milestones= ["3.7 M7", "3.7 RC1", "3.7", "3.8"];
+var target_milestones= ["3.7 RC1", "3.7 RC2", "3.7 RC3", "3.7 RC4", "3.7", "3.8"];
 
 var textCategories= [
 "-- Text category --",
