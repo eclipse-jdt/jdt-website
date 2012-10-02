f633ca4d9093b8eaa8f5bcd2d6cb6b675e8e09a8
mkeller <mkeller>
2012-10-02 16:23:04 +0000

    test modifiable configurations

---
+++ ui/scripts/jdtbugzilla.config.js
@@ -0,0 +1,10 @@
+// Configuration script for https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
+//
+// To override configuration options, just assign new values to the variables
+// that are defined at the beginning of the script. 
+
+/* Example (commented out):
+ccs= [
+"EX", "mail@example.com",
+]; 
+*/
