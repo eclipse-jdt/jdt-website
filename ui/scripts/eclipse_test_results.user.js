e1021fc494452e547f60ac196b0a077319888704
Markus Keller <markus_keller@ch.ibm.com>
2013-11-13 21:31:44 +0100

    add "Load test times" button

--- ui/scripts/eclipse_test_results.user.js
+++ ui/scripts/eclipse_test_results.user.js
@@ -14,13 +14,17 @@
 // @description   implements Bug 290883: Add links to XML test results
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/eclipse_test_results.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/eclipse_test_results.user.js
-// @version       1.20130715T1510
+// @version       1.20131113T2023
 
 // @include       http*://*/downloads/drops*/*/testResults.php
 // ==/UserScript==
 
 var anchors= document.getElementsByTagName("a");
 var htmlResultRegex= /testresults\/html\/(.*)\.html/; // <a href="testresults/html/org.eclipse.ant.tests.core_linux.gtk.x86.html">(0)</a>
+
+function getXmlRef(ref) {
+	return ref.replace(htmlResultRegex, "testresults/xml/$1.xml");
+}
 
 for (var i= 0; i < anchors.length; i++) {
     var aElem= anchors[i];
@@ -29,9 +33,42 @@ for (var i= 0; i < anchors.length; i++) {
         var xmlElem= document.createElement("a");
         xmlElem.textContent= "xml";
         xmlElem.setAttribute("style", "color:#AAAAAA");
-        xmlElem.href= aElem.href.replace(htmlResultRegex, "testresults/xml/$1.xml");
+        xmlElem.href= getXmlRef(aElem.href);
         xmlElem.title= "XML Test Result (e.g. for importing into the Eclipse JUnit view)";
         aElem.parentNode.appendChild(document.createTextNode(" "));
         aElem.parentNode.appendChild(xmlElem);
+    } else if (aElem.name == "UnitTest") {
+        var pElem= document.createElement("p");
+        pElem.style= "text-align:right;";
+        aElem.parentNode.parentNode.insertBefore(pElem, aElem.parentNode.nextElementSibling.nextElementSibling);
+        
+        var loadElem= document.createElement("button");
+        pElem.appendChild(loadElem);
+        loadElem.textContent= "Load test times";
+        loadElem.addEventListener("click", loadTestTimes, false);
     }
+}
+
+function loadTestTimes() {
+	for (var i= 0; i < anchors.length; i++) {
+		var aElem= anchors[i];
+		
+		if (aElem.href.search(htmlResultRegex) != -1) {
+			function appendTimeTo(myElem) { // JavaScript garbage to capture aElem
+				return function () { // function I want to assign to XMLHttpRequest::onload
+					var xml= this.responseXML;
+					var testsuites= xml.getElementsByTagName("testsuite");
+					for (var i= 0; i < testsuites.length; i++) {
+						var testsuite= testsuites[i];
+						var time= testsuite.getAttribute("time");
+						myElem.parentNode.appendChild(document.createTextNode(" " + time + "s"));
+					}
+				};
+			};
+			var r= new XMLHttpRequest();
+			r.onload= appendTimeTo(aElem); // JavaScript garbage to capture aElem
+			r.open("GET", getXmlRef(aElem.href), true);
+			r.send();
+		}
+	}
 }
