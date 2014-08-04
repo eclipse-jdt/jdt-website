2221982eda64424a0731522f3a403b343c82fdb2
Markus Keller <markus_keller@ch.ibm.com>
2014-08-04 15:41:20 +0200

    @include all pages from jdtbugzilla.user.js

--- ui/scripts/jdtbugzilla-start.user.js
+++ ui/scripts/jdtbugzilla-start.user.js
@@ -5,11 +5,16 @@
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla-start.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla-start.user.js
 
-// @include     https://bugs.eclipse.org/bugs/buglist.cgi*
-// @include     https://bugs.eclipse.org/bugs/show_bug.cgi*
-// @include     https://bugs.eclipse.org/bugs/process_bug.cgi*
+// @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
+// @include       https://bugs.eclipse.org/bugs/process_bug.cgi
+// @include       https://bugs.eclipse.org/bugs/attachment.cgi*
+// @include       https://bugs.eclipse.org/bugs/enter_bug.cgi*
+// @include       https://bugs.eclipse.org/bugs/post_bug.cgi*
+// @include       https://bugs.eclipse.org/bugs/query.cgi*
+// @include       https://bugs.eclipse.org/bugs/buglist.cgi*
+// @include       https://bugs.eclipse.org/bugs/show_activity.cgi*
 
-// @version 1.20140804T1417
+// @version 1.20140804T1540
 // @grant       none
 // @run-at document-start
 // ==/UserScript==
