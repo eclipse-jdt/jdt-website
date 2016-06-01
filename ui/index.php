4222ae9afe9f6ecf97745b938994f5ce57d192ea
Jay Arthanareeswaran <jarthana@in.ibm.com>
2016-06-01 19:26:13 +0530

    Bug 478356 - Update JDT web site

--- ui/index.php
+++ ui/index.php
@@ -9,7 +9,7 @@
 	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
 	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
 	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
+	$Nav->addCustomNav( "About This Project", "/projects/project_summary.php?projectid=eclipse.jdt.ui", "", 1  );
 	# End: page-specific settings
 	#
 		
