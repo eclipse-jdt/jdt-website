ba344313703e54e6acc867ac43486aa177fb2550
Markus Keller <markus_keller@ch.ibm.com>
2016-12-15 12:21:13 +0100

    4.7 M5

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -33,7 +33,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20161208T1357
+// @version 1.20161215T1220
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -61,7 +61,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.7 M4", "4.7 M5", "4.7", "4.6.3", "BETA J9"];
+var target_milestones= ["4.7 M5", "4.7 M6", "4.7", "4.6.3", "BETA J9"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
