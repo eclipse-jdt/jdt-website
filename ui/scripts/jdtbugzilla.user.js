fdcb0609e477b113f5c5529d4133f3bafc3bc27a
Noopur Gupta <noopur_gupta@in.ibm.com>
2018-01-25 16:59:59 +0530

    Update to 4.8 M6

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -33,7 +33,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20171207T0850
+// @version 1.20180125T1126
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -64,7 +64,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.8 M5", "4.7.3", "4.8", "BETA J18.3"];
+var target_milestones= ["4.8 M6", "4.7.3", "4.8", "BETA J18.3"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
