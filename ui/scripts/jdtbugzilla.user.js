3e2ed8fcda8d9a9a9858b4aeadb118183ea685b0
Noopur Gupta <noopur_gupta@in.ibm.com>
2018-03-08 17:49:09 +0530

    Add JDT categories for 10 and 11

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -33,7 +33,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20180308T0913
+// @version 1.20180308T1218
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -213,6 +213,8 @@ categories["Text"]= [
 categories["Text"].url= "http://www.eclipse.org/eclipse/platform-text/development/bug-annotation.htm";
 
 categories["JDT"]= [
+"[11]",
+"[10]",
 "[9]",
 "[1.8]",
 "[1.7]",
