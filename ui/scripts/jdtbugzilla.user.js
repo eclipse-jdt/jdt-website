d62275a583e438d45c0c7b747d209c6e34d654e3
Noopur Gupta <noopur_gupta@in.ibm.com>
2018-09-12 14:55:13 +0530

    Added TMs 4.10 M1 and 4.10 M3

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -33,7 +33,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20180903T0944
+// @version 1.20180912T0924
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -64,7 +64,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.10", "BETA J11", "BETA J12"];
+var target_milestones= ["4.10 M1", "4.10 M3", "4.10", "BETA J11", "BETA J12"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
