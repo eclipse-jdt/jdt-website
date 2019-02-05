dc603566fe376888d5f906d934a2f8b4a3166fca
Noopur Gupta <noopur_gupta@in.ibm.com>
2019-02-05 16:22:40 +0530

    Added TM "4.12"
    Change-Id: I706431b2fa66cd45487e5173bdba20f14ddd442e

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -34,7 +34,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20190111T0938
+// @version 1.20190205T1051
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -65,7 +65,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.11 M3", "4.11", "BETA J12", "BETA J13"];
+var target_milestones= ["4.11 M3", "4.11", "4.12", "BETA J12", "BETA J13"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
