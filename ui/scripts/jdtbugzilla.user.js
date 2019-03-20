2f0f6e5c1ed17e95948eeba56d4e5d4573b695f9
Noopur Gupta <noopur_gupta@in.ibm.com>
2019-03-20 16:21:24 +0530

    Added "4.12 M1", "4.12 M3" TMs
    Change-Id: If41300f81bfce9623ceb30e606728bf9f0ee089f

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -34,7 +34,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20190308T0800
+// @version 1.20190320T1050
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -65,7 +65,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.12", "BETA J12", "BETA J13"];
+var target_milestones= ["4.12 M1", "4.12 M3", "4.12", "BETA J12", "BETA J13"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
