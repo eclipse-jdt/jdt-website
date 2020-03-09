df88acb535292115fc85e48e8dd1c000e965517c
Noopur Gupta <noopur_gupta@in.ibm.com>
2020-03-09 14:49:32 +0530

    Remove TMs "4.15 RC2", "4.15"
    Change-Id: Ia7ee08de4ff2f88284f3ca2687444b2a0d5bfa70

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -34,7 +34,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20200228T0738
+// @version 1.20200309T0919
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -65,7 +65,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.16 M1", "4.16", "4.15 RC2", "4.15", "BETA J14"];
+var target_milestones= ["4.16 M1", "4.16", "BETA J14"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
