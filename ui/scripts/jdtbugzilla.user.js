0c5bfbe94ab52704d25062d11e46a756ba2e366b
Noopur Gupta <noopur_gupta@in.ibm.com>
2020-10-20 15:01:00 +0530

    Removed TM "4.18 M1"
    Change-Id: I43d003d83b1966b15653daeb5a4256075bf3fa71

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -34,7 +34,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20201001T1041
+// @version 1.20201020T0929
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -65,7 +65,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.18 M1", "4.18 M3", "4.18"];
+var target_milestones= ["4.18 M3", "4.18"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
