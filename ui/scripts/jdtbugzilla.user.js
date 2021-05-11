1064f45e4cf130e4e90e069ea7ea3d8563a4758b
Noopur Gupta <noopur_gupta@in.ibm.com>
2021-05-11 15:07:52 +0530

    Add TMs "4.20 M2", "4.20 M3", "4.20 RC1", "4.20 RC2"
    Change-Id: Ic66950fdc91971f423e2094ec4a1ccd6a2a7d87e

--- ui/scripts/jdtbugzilla.user.js
+++ ui/scripts/jdtbugzilla.user.js
@@ -34,7 +34,7 @@
 // @resource      config   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.config.js
 // @downloadURL   https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
 // @updateURL     https://www.eclipse.org/jdt/ui/scripts/jdtbugzilla.user.js
-// @version 1.20210420T1043
+// @version 1.20210511T0937
 
 // @include       https://bugs.eclipse.org/bugs/show_bug.cgi*
 // @include       https://bugs.eclipse.org/bugs/process_bug.cgi
@@ -65,7 +65,7 @@
 // - edit jdtbugzilla.config.js
 
 // Add as many milestones as you like:
-var target_milestones= ["4.20 M1", "4.20", "BETA J17"];
+var target_milestones= ["4.20 M2", "4.20 M3", "4.20 RC1", "4.20 RC2", "4.20", "BETA J17"];
 
 // Indexes into target_milestones to be used for "Fixed (in <TM>)" links
 var main_target_milestones= [0/*, 2*/];
